07a4ad26b4d414c70ef6c8bbd3c19e8bf9ff9988
dboren <danielboren@att.net>
2021-03-09 15:13:00 -0600

    added init function with prompts to enter manager info

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const inquirer = require("./inquirer");
+const inquirer = require("inquirer");
+
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Enter the name of the team manager:'
+            },
+            {
+                type: 'input',
+                name: 'ID',
+                message: 'Enter the ID number of the team manager:'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Enter the e-mail address of the team manager:'
+            },
+            {
+                type: 'input',
+                name: 'ofcNum',
+                message: 'Enter the office number of the team manager:'
+            }
+        ])
+}
+
+init();
