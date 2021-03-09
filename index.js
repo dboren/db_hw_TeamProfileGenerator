c229a544fcd3dcdf8950b0ec93d0f69526918e80
dboren <danielboren@att.net>
2021-03-09 12:05:53 -0600

    created initial Employee class

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const inquirer = require("./inquirer");
 
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+        console.log(`Name: ${this.name}`);
+    }
+    getID() {
+        console.log(`ID: ${this.id}`);
+    }
+    getEmail() {
+        console.log(`E-mail address: ${this.email}`);
+    }
+    // getRole() {
+
+    // }
+}
