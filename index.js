f7d0186bf5aea90314799983db5ac207662324f0
dboren <danielboren@att.net>
2021-03-10 15:35:49 -0600

    moved mgr prompts to their own variable to clean up init function

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const inquirer = require("inquirer");
 
+const mgPrompts = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter the name of the team manager:'
+    },
+    {
+        type: 'input',
+        name: 'ID',
+        message: 'Enter the ID number of the team manager:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter the e-mail address of the team manager:'
+    },
+    {
+        type: 'input',
+        name: 'ofcNum',
+        message: 'Enter the office number of the team manager:'
+    }
+];
+
 function init() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'Enter the name of the team manager:'
-            },
-            {
-                type: 'input',
-                name: 'ID',
-                message: 'Enter the ID number of the team manager:'
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: 'Enter the e-mail address of the team manager:'
-            },
-            {
-                type: 'input',
-                name: 'ofcNum',
-                message: 'Enter the office number of the team manager:'
-            }
-        ])
+    inquirer.prompt(mgPrompts)
 }
 
 init();
